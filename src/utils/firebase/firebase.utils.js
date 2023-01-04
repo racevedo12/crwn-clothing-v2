6a8a178ceee5722033ac95728e90e13ec1082321
racevedo12 <ronny.acv2@hotmail.com>
2023-01-04 11:57:58 -0500

    Created a Sign In Form Component, and modified old Sign In Component into Authentication Component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,7 @@ import
     signInWithPopup,
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
 
 } from "firebase/auth";
 
@@ -96,6 +97,17 @@ export const createAuthUserWithEmailAndPassword = async (email, password) =>
     return;
   }
 
-  createAuthUserWithEmailAndPassword(auth, email, password);
+  return await createUserWithEmailAndPassword(auth, email, password);
+  
+};
+
+export const SignInAuthUserWithEmailAndPassword = async (email, password) =>
+{
+  if (!email || !password)
+  {
+    return;
+  }
+
+  return await signInWithEmailAndPassword(auth, email, password);
   
 };
